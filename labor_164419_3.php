b9e8b583cdaa4e4ad43a87d63ce8c8b7c53101de
Jakub Pisarski <92027575+JPisarski@users.noreply.github.com>
2023-10-26 16:21:51 +0200

    Add files via upload

---
+++ labor_164419_3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?
+	$nr_indeksu = '164419';
+	$nrGrupy = '3';
+	
+	echo 'Jakub Pisarski '.$nr_indeksu.' grupa'.$nrGrupy.' <br/><br/>;
+	
+	echo `Zastosowanie metody include()<br/>';
+?>
+</body>
+</html>
