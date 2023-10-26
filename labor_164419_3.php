17122fe678d901b13de3c520230a44c4158a3346
Jakub Pisarski <92027575+JPisarski@users.noreply.github.com>
2023-10-27 00:55:24 +0200

    Update labor_164419_3.php

--- labor_164419_3.php
+++ labor_164419_3.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
-<html>
-<body>
 
-<?
-	$nr_indeksu = '164419';
-	$nrGrupy = '3';
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $nr_indeksu = '164419';
+            $nrGrupy = '3';
 	
-	echo 'Jakub Pisarski '.$nr_indeksu.' grupa'.$nrGrupy.' <br/><br/>;
+            echo 'Jakub Pisarski '.$nr_indeksu.' grupa'.$nrGrupy.' <br/><br/>';
 	
-	echo `Zastosowanie metody include()<br/>';
-?>
-</body>
+            echo 'Zastosowanie metody include()<br/>';
+        ?>
+    </body>
 </html>
