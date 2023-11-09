1502181add5fd2b750055d9581ae52beaf545bbb
Jakub Pisarski <92027575+JPisarski@users.noreply.github.com>
2023-11-09 16:24:33 +0100

    Add files via upload

---
+++ lab5/index.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" /> 
+	<meta http-equiv="Content-Language" content="pl" /> 
+	<meta name="Author" content="Jakub Pisarski" /> 
+	<title>Moje hobby to genealogia</title> 
+	<link rel="stylesheet" href="css/style.css" />	
+	</style>
+</head>
+<body>
+	<table>
+		<tr>
+			<td><a href="index.html"><b><i>Strona główna</i></b></a></td>
+			<td><a href="html/Dlaczego_genealogia.html"><b><i>Dlaczego genealogia?</i></b></a></td>
+			<td><a href="html/Gdzie_szukac.html"><b><i>Gdzie szukać?</i></b></a></td>	
+			<td><a href="html/Na_co_uwazac.html"><b><i>Na co uważać?</i></b></a></td>
+			<td><a href="html/Drzewo_genealogiczne.html"><b><i>Drzewo genealogiczne</i></b></a></td>
+			<td><a href="html/Historia_mojej_rodziny.html"><b><i>Historia mojej rodziny</i></b></a></td>
+			<td><a href="html/Kontakt.html"><b><i>Kontakt</i></b></td>
+		</tr>
+	</table>	
+	<div class="naglowek"> <h1><i>Serdecznie witam na mojej stronie!</i></h1> </div>
+	<div class="wpr"><p>Nazywam się <u>Jakub Pisarski</u>. Od ponad 5 lat zajmuję się hobbysticzne genealogią. Na tej stronie znajdziesz wskazówki, jak prowadzić badania genealogiczne oraz moje doświadczenie związane z poszukiwaniami przodków.</div>
+	<div class="naglowek"><h2><i>O genealogii słów kilka</i></h2></div>
+	<img src="img/drzewko.jpg" width="500">
+	<div class="tresc"><p><span class="kolorek"><b>Genealogia</b></span> (z gr. genos – „ród” oraz λόγος, logos – „słowo”, „wiedza”) to jedna z nauk pomocniczych historii, zajmująca się badaniem więzi rodzinnych między ludźmi na bazie zachodzącego między nimi pokrewieństwa i powinowactwa. W szczególności przedmiotem zainteresowania genealogii są wybrane rodziny i rody, ich pochodzenie, historia oraz wzajemne relacje rodzinne i losy poszczególnych członków rodziny.</p>
+	<p>Badania genealogiczne prowadzi się na podstawie swoistych źródeł, w tym zwłaszcza metryk. Po ustaleniu niezbędnych faktów genealogicznych sporządza się tablice genealogiczne ukazujące relacje między członkami rodziny (np. drzewo genealogiczne). Efekt badań może przyjąć również postać innych publikacji papierowych (np. książka, skrypt, artykuł), a współcześnie także elektronicznych (np. w programie genealogicznym, na stronie internetowej). Publikacje takie często są utożsamiane z genealogią (genealogia danej rodziny).</p>
+	<p>Prowadzeniem badań genealogicznych zajmuje się genealog. Może to być genealog zawodowy, traktujący genealogię jako profesję, lub genealog amator, zajmujący się genealogią rodzinną jako hobby. W Polsce genealogia stanowi jedynie element nauczania na studiach wyższych na kierunku – historia.</p>
+	<p>W przeszłości przedmiotem zainteresowań były głównie dynastie panujące i rody szlacheckie, ale współcześnie podejmuje się badania rodzin pochodzących ze wszystkich stanów społecznych. Genealodzy w szczególności zajmują się rodziną własną, jak również rodzinami związanymi z jakimś obszarem (np. pochodzącymi z danej miejscowości) lub zdarzeniem (np. uczestnicy doniosłego zdarzenia, imigranci).</p>
+	<p>Od II poł. XX w. na świecie, w tym w Polsce od końca lat 90., obserwuje się intensywny wzrost zainteresowania własną, indywidualną genealogią rodzinną i poszukiwaniem przodków, co przejawia się zwłaszcza w powstawaniu towarzystw genealogicznych (w tym Polskiego Towarzystwa Genealogicznego w 2006), w zjazdach rodzinnych czy w tworzeniu rodzinnych witryn internetowych.</p>
+	<p>Podstawą genealogii jest istnienie różnie nazywanych więzi rodzinnych (w różnych wyrażeniach: więzy, więzi, węzły, relacje, stosunki, związki – krwi, rodzinne, genealogiczne). Stosunki genealogiczne pomiędzy osobami co do zasady mają charakter biologiczny (genetyczny, naturalny, faktyczny). Na tej podstawie powszechnie przyjmuje się, że źródłem więzów krwi jest filiacja rozumiana jako pochodzenie zstępnych dzieci od pary wstępnych rodziców. Rodzinę tworzą również osoby związane poprzez koicję, tj. związek (małżeński lub nieślubny) kobiety i mężczyzny celem posiadania potomstwa. Prawnym odpowiednikiem genealogicznej filiacji i koicji są odpowiednio pokrewieństwo zachodzące między osobami posiadającymi co najmniej jednego wspólnego przodka oraz powinowactwo zachodzące między krewnymi jednego małżonka a drugim małżonkiem. Pokrewieństwo i powinowactwo oblicza się w stopniach i liniach.</p></div>
+      
+        <?php
+        $nr_indeksu = '164419';
+        $nrGrupy = '3';
+        
+        echo 'Autor: Jakub Pisarski '.$nr_indeksu.' grupa '.$nrGrupy.' <br /><br />';
+        ?>
+
+</body>
+</html>
