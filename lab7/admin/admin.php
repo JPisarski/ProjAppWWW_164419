dec521cdbb963741d37cec8a9a705d22ea980213
Jakub Pisarski <92027575+JPisarski@users.noreply.github.com>
2023-12-19 05:02:55 +0100

    Add files via upload

--- lab7/admin/admin.php
+++ lab7/admin/admin.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="../css/style_admin.css">
+
 <?php
 
 
@@ -7,16 +9,14 @@ function FormularzLogowania()
 {
     $wynik = '
     <div class="logowanie">
-        <h1 class="heading">Panel CMS:</h1>
-        <div class="logowanie">
+        <h1 class="naglowek">Panel CMS</h1>
             <form method="post" name="LoginForm" enctype="multipart/form-data" action="' . $_SERVER['REQUEST_URI'] . '">
-                <table class="logowanie">
-                    <tr><td class="kog4_t">[email]</td><td><input type="text" name="login" class="logowanie" /></td></tr>
-                    <tr><td class="log4_t">[haslo]</td><td><input type="password" name="pass" class="logowanie" /></td></tr>
-                    <tr><td>&nbsp;</td><td><input type="submit" name="x1_submit" class="logowanie" value="zaloguj" /></td></tr>
+                <table class="">
+                    <tr><td class="kog4_t">Login: </td><td><input type="text" name="login" class="" /></td></tr>
+                    <tr><td class="log4_t">Hasło: </td><td><input type="password" name="pass" class="" /></td></tr>
+                    <tr><td>&nbsp;</td><td><input type="submit" name="x1_submit" class="" value="Zaloguj się" /></td></tr>
                 </table>
             </form>
-        </div>
     </div>
     ';
 
@@ -30,10 +30,16 @@ function ListaPodstron()
     if (!isset($_SESSION['status']) || $_SESSION['status'] == 1) {
         $query = "SELECT * FROM page_list ORDER BY id ASC";
         $result = mysqli_query($link, $query);
-
-        while ($row = mysqli_fetch_array($result)) {
-            echo $row['id'] . ' ' . $row['page_title'].' <a href="admin.php?funkcja=usun&id='.$row['id'].'">Usuń</a> <a href="admin.php?funkcja=edytuj&id='.$row['id'].'">Edytuj</a>  <br />';
-        }
+		echo '<h1 class="naglowek">Lista podstron</h1><center><table>';
+		if($result){
+			while ($row = mysqli_fetch_array($result)) {
+				echo '<tr><td class="tdid"><b>'.$row['id'] . '<b></td><td class="tdnazwa"><b>'. $row['page_title'].'<b></td><td class="tdusun"><a href="admin.php?funkcja=usun&id='.$row['id'].'"><b>Usuń</b></a></td><td class="tdedytuj"><a href="admin.php?funkcja=edytuj&id='.$row['id'].'"><b>Edytuj</b></a></td></tr>';
+			}
+			echo '</table></center><br>';
+		}
+		else{
+			echo "Brak stron";
+		}
     }
 	if(isset($_GET['funkcja']) && $_GET['funkcja'] == 'usun'){
 		UsunPodstrone();
@@ -42,11 +48,7 @@ function ListaPodstron()
 
 		EdytujPodstrone();
 	}
-	echo FormularzDodawania();
 	DodajNowaPodstrone();
-	
-	
-	
 }
 
 
@@ -60,18 +62,16 @@ function EdytujPodstrone()
 	$result = mysqli_query($link ,$query);
 	$row = mysqli_fetch_array($result);
 	echo '
-    <div class="edycja">
-        <h1 class="heading"><b>Edytuj podstronę<b/></h1>
-        <div class="edycja">
+    <div class="">
+        <h1 class="naglowek"><b>Edytuj podstronę<b/></h1>
             <form method="post" name="EditForm" enctype="multipart/form-data" action="' . $_SERVER['REQUEST_URI'] . '">
                 <table class="edycja">
-                    <tr><td class="edit_4t"><b>Tytuł podstrony: <b/></td><td><input type="text" name="page_title" class="edycja" value='.$row['page_title'].' /></td></tr>
-                    <tr><td class="edit_4t"><b>Treść podstrony: <b/></td><td><textarea rows=50 cols=100 name="page_content" class="edycja" />'.$row['page_content'].'</textarea></td></tr>
-                    <tr><td class="edit_4t"><b>Status podstrony: <b/></td><td><input type="checkbox" name="status" class="edycja" /></td></tr>
+                    <tr><td class="edit_4t"><b>Tytuł podstrony: <b/></td><td><input type="text" name="page_title" size="108" value='.$row['page_title'].' /></td></tr>
+                    <tr><td class="edit_4t"><b>Treść podstrony: <b/></td><td><textarea rows=20 cols=100 name="page_content"/>'.$row['page_content'].'</textarea></td></tr>
+                    <tr><td class="edit_4t"><b>Status podstrony: <b/></td><td><input type="checkbox" name="status" checked /></td></tr>
                     <tr><td>&nbsp;</td><td><input type="submit" name="x2_submit" class="edycja" value="Edytuj" /></td></tr>
                 </table>
             </form>
-        </div>
     </div>
     ';
     if (isset($_POST['x2_submit'])&& isset($_GET['id'])) {
@@ -85,42 +85,33 @@ function EdytujPodstrone()
 
             $result = mysqli_query($link, $query);
 
-            if ($result) {
-                echo "Edycja zakończona pomyślnie!";
-                header("Location: admin.php");
+            if ($result) {  
+                echo "<script>window.location.href='admin.php';</script>";
                 exit();
             } else {
-                echo "Błąd podczas edycji: " . mysqli_error($link);
+                echo "<center>Błąd podczas edycji: " . mysqli_error($link)."</center>";
             }
         }
     }
 }
 
 
-function FormularzDodawania()
-{
-    $add = '
-    <div class="dodaj">
-        <h1 class="heading"><b>Dodaj podstronę<b/></h1>
-        <div class="dodaj">
-            <form method="post" name="AddForm" enctype="multipart/form-data" action="' . $_SERVER['REQUEST_URI'] . '">
-                <table class="dodaj">
-                    <tr><td class="add_4t"><b>Tytuł podstrony: <b/></td><td><input type="text" name="page_title_add" class="dodaj" /></td></tr>
-                    <tr><td class="add_4t"><b>Treść podstrony: <b/></td><td><input type="text" name="page_content_add" class="dodaj" /></td></tr>
-                    <tr><td class="add_4t"><b>Status podstrony: <b/></td><td><input type="checkbox" name="status_add" class="dodaj" /></td></tr>
-                    <tr><td>&nbsp;</td><td><input type="submit" name="x3_submit" class="dodaj" value="dodaj" /></td></tr>
-                </table>
-            </form>
-        </div>
-    </div>
-    ';
-
-    return $add;
-}
-
 function DodajNowaPodstrone()
 {
     global $link;
+	echo '
+    <div class="">
+        <h1 class="naglowek"><b>Dodaj podstronę<b/></h1>
+            <form method="post" name="AddForm" enctype="multipart/form-data" action="' . $_SERVER['REQUEST_URI'] . '">
+                <table class="dodaj">
+                    <tr><td class="add_4t"><b>Tytuł podstrony: <b/></td><td><input type="text" name="page_title_add" size="108"/></td></tr>
+                    <tr><td class="add_4t"><b>Treść podstrony: <b/></td><td><textarea rows=20 cols=100 name="page_content_add" /></textarea></td></tr>
+                    <tr><td class="add_4t"><b>Status podstrony: <b/></td><td><input type="checkbox" name="status_add" checked /></td></tr>
+                    <tr><td>&nbsp;</td><td><input type="submit" name="x3_submit" class="dodaj" value="Dodaj" /></td></tr>
+                </table>
+            </form>
+    </div>
+    ';
     if (isset($_POST['x3_submit'])) {
         $tytul = $_POST['page_title_add'];
         $tresc = $_POST['page_content_add'];
@@ -129,12 +120,11 @@ function DodajNowaPodstrone()
         $query = "INSERT INTO page_list (page_title, page_content, status) VALUES ('$tytul', '$tresc', $status)";
         $result = mysqli_query($link, $query);
 
-        if ($result) {
-            echo "Pomyślnie dodano podstronę!";
-            header("Location: admin.php");
+        if ($result) {           
+            echo "<script>window.location.href='admin.php';</script>";
             exit();
         } else {
-            echo "Błąd podczas dodawania podstrony: " . mysqli_error($link);
+            echo "<center>Błąd podczas dodawania podstrony: " . mysqli_error($link)."</center>";
         }
     }
 }
@@ -150,21 +140,20 @@ function UsunPodstrone()
         $query = "DELETE FROM page_list WHERE id = $id LIMIT 1";
         $result = mysqli_query($link, $query);
 
-        if ($result) {
-            echo "Pomyślnie usunięto podstronę!";
-            header("Location: admin.php");
+        if ($result) {         
+            echo "<script>window.location.href='admin.php';</script>";
             exit();
         } else {
-            echo "Błąd podczas usuwania podstrony: " . mysqli_error($link);
+            echo "<center>Błąd podczas usuwania podstrony: " . mysqli_error($link)."</center>";
         }
     }
 }
 
 
 if(isset($_SESSION['status_logowania']) && $_SESSION['status_logowania'] == 1){
-	echo 'Jesteś zalogowany i masz dostęp do metod administracyjnych. </br>';
+	echo '<center><br>Jesteś zalogowany i masz dostęp do metod administracyjnych. <br><br></center>';
 	ListaPodstron();
-	echo '<a href="wyloguj.php">Wyloguj się</a>';
+	echo '<h2 class=naglowek><a href="wyloguj.php">Wyloguj się</a></h2>';
 } 
 else {
 	echo FormularzLogowania();
@@ -177,7 +166,7 @@ if(isset($_POST['login']) && isset($_POST['pass']))
 		header("Location: admin.php");
 	}
 	else{
-		echo 'Błędne dane. Spróbuj jeszcze raz.';
+		echo '<center><br><br><br><br><br>Wprowadzono niepoprawne dane! <br><br>Spróbuj zalogować się ponownie.</center>';
 	}
 }
 
