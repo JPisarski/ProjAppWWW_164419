458946e8ad16bf479a4a2dd20c4709353de6f997
Jakub Pisarski <92027575+JPisarski@users.noreply.github.com>
2023-12-31 01:03:24 +0100

    Add files via upload

--- lab9/contact.php
+++ lab9/contact.php
@@ -1,102 +1,139 @@
+<link rel="stylesheet" href="./css/style_admin.css">
+
 <?php
 
-include ('../cfg.php');
+// dołączamy zawartość pliku cfg.php
 
-session_start();
+include('cfg.php');
+
+// importujemy klasy PHPMailer do globalnej przestrzeni nazw
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+require 'PHPMailer-master/src/Exception.php';
+
+// funkcja PokazKontakt() wyświetla formularz, który jest potrzebny do wysłania maila 
 
 function PokazKontakt()
 {
-    echo'
-    <div class="Kontakt">
-        <h1 class="heading">poczta:</h1>
-        <div class="formularz">
-            <form method="post" name="mail" enctype="multipart/form-data" action="' . $_SERVER['REQUEST_URI'] . '">
-                <table class="formularz">
-                    <tr><td class="kon4_t">Temat:</td><td><input type="text" name="kontakt" class="formularz" /></td></tr>
-                    <tr><td class="kon4_t">Wiadomość:</td><td><input type="text" name="wiadomosc" class="formularz" /></td></tr>
-                    <tr><td class="kon4_t">Nadawca:</td><td><input type="text" name="email" class="formularz" /></td></tr>
-                    <tr><td>&nbsp;</td><td><input type="submit" name="x4_submit" class="kontakt" value="wyslij" /></td></tr>
+	echo '
+    <div><br>
+        <h1 class="naglowek">Wyślij mail</h1>
+            <form method="post" action="'.$_SERVER['REQUEST_URI'].'">
+                <table>
+					<tr><td>Nadawca:</td><td><input type="text" name="nadawca" size="50" required/></td></tr>
+                    <tr><td>E-mail:</td><td><input type="text" name="adres" size="50" required/td></tr>
+					<tr><td>Temat:</td><td><input type="text" name="temat" size="50" required/></td></tr>
+                    <tr><td>Wiadomość:</td><td><textarea name="tresc" rows=15 cols=47 required></textarea></td></tr>                 
+                    <tr><td></td><td><input type="submit" name="wyslij_mail" value="Wyślij mail" /></td></tr>
                 </table>
             </form>
-        </div>
     </div>
-    ';
-
+	';
 }
 
-function WyslijMailKontakt($odbiorca)
+// funkcja WyslijMailKontakt() wysyła mail utworzony za pomocą danych zawartych w powyższym formularzu 
+
+function WyslijMailKontakt()
 {
-	if(empty($_POST['temat']) || empty($_POST['wiadomosc']) || empty($_POST['email']))
-	{
-		echo '[nie_wypelniles_pola]';
-		PokazKontakt();
-	}
-	else
-	{
-		$mail['subject'] = $_POST['temat'];
-		$mail['body'] = $_POST['wiadomosc'];
-		$mail['sender'] = $_POST['email'];
-		$mail['reciptient'] = $odbiorcal
+	global $config;
+	PokazKontakt();
+	
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['wyslij_mail'])) {
+		$mail = new PHPMailer(true);
 		
-		$header .= "From: Formularz kontaktowy <".$mail['sender'].">\n";
-		$header .= "MIMIE-Version: 1.0\nContent-Type: text/plain; charset=utf-8\nContent-Transfer-Encoding: \n";
-		$header .= "X-Sender : <".$mail['sender'].">\n";
-		$header .= "X-Mailer: PRapWWW mail 1.2\n";
-		$header .= "X-Priority: 3\n";
-		$header .= "Return-Path: <".$mail['sender'].">\n";
-		
-		mail($mail['reciptient'],$mail['subject'],$mail['body'],$header);
-		
-		echo '[wiadomosc_wyslana]';
-	}
+        try {
+			$mail->CharSet = "UTF-8";
+            $mail->SMTPDebug = 0; 
+            $mail->isSMTP();
+            $mail->Host = $config['smtp_host'];
+            $mail->SMTPAuth = $config['smtp_auth'];
+            $mail->Username = $config['smtp_username'];
+            $mail->Password = $config['smtp_password'];
+            $mail->SMTPSecure = $config['smtp_secure'];
+            $mail->Port = $config['smtp_port'];
+            $mail->setFrom($_POST['adres'], $_POST['nadawca']);
+			$mail->AddReplyTo($_POST['adres'], $_POST['nadawca']);
+            $mail->addAddress("jn242031@gmail.com");
+            $mail->isHTML(false);
+            $mail->Subject = $_POST['temat'];
+            $mail->Body = $_POST['tresc'];
+			
+			// wysyłanie maila
+			
+            $mail->send();
+			echo "<script>window.location.href='contact.php';</script>";
+			exit();
+        } catch (Exception $e) {
+             echo '<center>Wiadomość nie została wysłana.</center>';
+        }
+    } 
 }
+
+// funkcja PrzypomnijHaslo() wyświetla formularz, a następny wysyła na podany w wcześniej wymienionym formularzu
+// adres e-mail wiadomość zawierającą zapomniane hasło
 
 function PrzypomnijHaslo()
 {
-	if(empty($_POST['email']))
-	{
-		echo '[nie_wypelniles_pola]';
-		echo '
-		<div class="Haslo">
-			<h1 class="heading">poczta:</h1>
-			<div class="formularzZapomniane">
-				<form method="post" name="mail" enctype="multipart/form-data" action="' . $_SERVER['REQUEST_URI'] . '">
-					<table class="formularz">
-						<tr><td class="for4_t">email:</td><td><input type="text" name="emailz" class="formularzZapomniane" /></td></tr>
-						<tr><td>&nbsp;</td><td><input type="submit" name="x5_submit" class="formularzZapomniane" value="wyslij" /></td></tr>
-					</table>
-				</form>
-			</div>
-		</div>
-    ';
-	}
-	else
-	{
-		$query = "SELECT haslo FROM tabela_uzytkownikow WHERE email =".$_POST['emailz']" LIMIT 1";
-        $wynik = mysqli_query($link, $query);
-		if (mysqli_num_rows($wynik) > 0) 
+	global $login;
+	global $pass;
+	global $config;
+	
+	echo '
+	<div><br>
+        <h1 class="naglowek">Przypomnij hasło</h1>
+            <form method="post" action="'.$_SERVER['REQUEST_URI'].'">
+                <table>
+					<tr><td>Login:</td><td><input type="text" name="login" required /></td></tr>
+                    <tr><td>E-mail:</td><td><input type="text" name="emaild" required/></td></tr>
+                    <tr><td></td><td><input type="submit" name="przypomij_haslo" value="Przypomnij hasło" /></td></tr>
+                </table>
+            </form>
+    </div>
+	';
+	
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['przypomij_haslo'])) {
+		if($login == $_POST['login'])
 		{
-			$row = mysqli_fetch_assoc($wynik);
-            $haslo = $row['haslo'];
-			$mail['subject'] = 'Haslo';
-			$mail['body'] = $_POST['wiadomosc'];
-			$mail['sender'] = $_POST['emailz'];
-			$mail['reciptient'] = $_POST['emailz'];
-		
-		
-			$header = "From: Formularz kontaktowy <".$mail['sender'].">\n";
-			$header .= "MIMIE-Version: 1.0\nContent-Type: text/plain; charset=utf-8\nContent-Transfer-Encoding: \n";
-			$header .= "X-Sender : <".$mail['sender'].">\n";
-			$header .= "X-Mailer: PRapWWW mail 1.2\n";
-			$header .= "X-Priority: 3\n";
-			$header .= "Return-Path: <".$mail['sender'].">\n";
-		
-			mail($mail['reciptient'],$mail['subject'],$mail['body'],$header);
-		
-		echo '[wiadomosc_wyslana]';
+			$mail = new PHPMailer(true);
+	
+			try {
+				$mail->CharSet = "UTF-8";
+				$mail->SMTPDebug = 0;
+				$mail->isSMTP();
+				$mail->Host = $config['smtp_host'];
+				$mail->SMTPAuth = $config['smtp_auth'];
+				$mail->Username = $config['smtp_username'];
+				$mail->Password = $config['smtp_password'];
+				$mail->SMTPSecure = $config['smtp_secure'];
+				$mail->Port = $config['smtp_port'];
+				$mail->setFrom('jn242031@gmail.com', 'Wiadomość przypominająca hasło');
+				$mail->addAddress($_POST['emaild']);
+				$mail->isHTML(false);
+				$mail->Subject = 'Przypomnienie hasła';
+				$mail->Body = 'Twoje hasło: ' . $pass;
+				
+				//wysyłanie maila
+				
+				$mail->send();
+				echo "<script>window.location.href='contact.php';</script>";
+				exit();
+			} catch (Exception $e) {
+				 echo '<center>Wiadomość nie została wysłana.</center>';
+			}
+		}
+		else{
+			echo '<center>Niepoprawny login! Wprowadź poprawny login.</center>';
+		}
 	}
 }
 
+// wywołujemy funkcje WyslijMailKontakt() i PrzypomnijHaslo()
 
+WyslijMailKontakt();
+PrzypomnijHaslo();
 
 ?>
